cbe078fc925b4f12a31a5bd7a6647a38fd01ce61
gabriel <gabrielgoller123@gmail.com>
2020-11-18 23:28:47 +0100

    📦️ Updated the project structure and backend

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "frontend", "public")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Server available at port: " + port);
